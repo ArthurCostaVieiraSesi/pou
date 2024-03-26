2f6a20cd2c9272d647cdd49620599aecbfeef7bc
ArthurCosta <opsarthur@gmail.com>
2024-03-26 07:37:45 -0300

    começo do projeto <3

---
+++ pou/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NomeDeProjeto;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
